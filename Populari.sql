628b734ef48b1b2927cefb4d4c9b2417156c2919
tandura <phi_brain1000@yahoo.com>
2016-01-09 14:00:30 +0200

    am inserat val default pentru testing

---
+++ Populari.sql
@@ -0,0 +1,11 @@
+insert into autor(Nume)
+values('Miron Costin'),('Grigore Ureche'),('Ion Neculce'),('Gheorghe Șincai'),('Ion Heliade Rădulescu'),('Vasile Alecsandri'),('Titu Maiorescu'),('Mihai Eminescu'),('Ion Creangă');
+
+insert into colectii(Nume)
+values('Adevarul'),('Biblioteca pentru toti'),('Colectionarului'),('Nautilus'),('Nobel'),('Curtea veche');
+
+insert into editura(Nume)
+values('Adantis'),('Babel'),('Cartea copiilor'),('Codex'),('Dacia'),('Pi'),('Euristica'),('Galaxia copiilor'),('Integral');
+
+insert into gen(Nume)
+values('Actiune'),('Istoric'),('Drama'),('Comedie'),('S.F.'),('Literatura Clasica'),('Poezie');
